9d3610775e47583c15767078a82b57edd4791988
Swn <2387452337@qq.com>
2018-04-21 19:47:28 +0800

    jd

---
+++ js/HDbuslide.js
@@ -0,0 +1,27 @@
+(function($){
+	$.fn.HDbuslide=function(settings){
+		settings=jQuery.extend({
+			"time":300,
+			 "speed":200
+		},settings)
+		$.fn.HDbuslide.show($(this),settings);
+	};
+	$.fn.HDbuslide.show=function($this,settings){
+		 var _list_w=$this.width();
+         t="",
+         _slide_nav=$this.find('.slide-nav li');
+         _slide_nav.hover(function(){
+         	var _left=$(this).index()*(-1)*_list_w,
+         	    _this=$(this);
+         	    setTimeout(function(){
+         	    	$this.children('.slide-ul').animate({
+         	    		"left":_left
+         	    	},settings.speed);
+         	    	_slide_nav.removeClass('slide-nav-curr');
+         	    	_this.addClass('slide-nav-curr');
+         	    },settings.time);
+         },function(){
+         	clearTimeout(t);
+         });
+	}
+})(jQuery)
